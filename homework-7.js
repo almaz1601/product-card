e68b2009bec70b7905d79a4a4c96a19ecbf0555a
алмаз <almazfa.86@gmail.com>
2026-08-06 08:45:31 +0300

    fixed errors

--- homework-7.js
+++ homework-7.js
@@ -1,43 +1,43 @@
-function showWeather(city,
-  temperature) {
+function showWeather(city, temperature) {
   console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
 }
 
 showWeather('Kazan', 25);
 
 
-const lightSpeed = 300000000
+const LIGHT_SPEED = 300000000
 
 function checkSpeed(speed) {
-  if (speed > lightSpeed) {
-    console.log('Cверхсветовая скорость')
-  } else if (speed < lightSpeed) {
-    console.log('Субсветовая скорость')
+  if (speed > LIGHT_SPEED) {
+    console.log('Cверхсветовая скорость');
+  } else if (speed < LIGHT_SPEED) {
+    console.log('Субсветовая скорость');
   } else {
-    console.log('Скорость света')
+    console.log('Скорость света');
   }
 }
-checkSpeed(301101000)
-checkSpeed(295292450)
-checkSpeed(300000000)
+
+checkSpeed(301101000);
+checkSpeed(295292450);
+checkSpeed(300000000);
 
 let product = 'Laptop';
 let price = 1500;
 
 function buyProduct(product) {
   if (product >= price) {
-    console.log(`${"Laptop"} оплачен. Спасибо за покупку!`)
+    console.log(`${"Laptop"} оплачен. Спасибо за покупку!`);
   } else {
-    console.log(`У вас не хватает денежных средств ${price - product}$, пополните баланс`)
+    console.log(`У вас не хватает денежных средств ${price - product}$, пополните баланс`);
   }
 }
 
-buyProduct(1500)
-buyProduct(1400)
+buyProduct(1500);
+buyProduct(1400);
 
 
 function sayHello() {
-  console.log("Привет!")
+  console.log("Привет!");
 }
 
 sayHello();
